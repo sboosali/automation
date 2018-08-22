0be55a55e5f9564a223953f341d65001fbc2bf42
Spiros Boosalis <SamBoosalis@gmail.com>
2018-08-21 19:25:42 -0700

    ./xdotool 1st

---
+++ xdotool/sources/Xdotool.hs
@@ -0,0 +1,27 @@
+--------------------------------------------------
+--------------------------------------------------
+
+{-|
+
+
+
+-}
+
+module Xdotool
+ ( module Xdotool.Core
+ , module Xdotool.Commands.Key
+ , module Xdotool.Commands.Type
+ , module Xdotool.Commands.Search
+ , module Xdotool.Options.Global
+ ) where
+
+--------------------------------------------------
+
+import Xdotool.Core
+import Xdotool.Commands.Key
+import Xdotool.Commands.Type
+import Xdotool.Commands.Search
+import Xdotool.Options.Global
+
+--------------------------------------------------
+--------------------------------------------------
